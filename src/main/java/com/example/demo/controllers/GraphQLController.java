4e199e60776423ed7d512d7ecf136179ae5ea037
Pavan Ganesh <pavanganesh2804@gmail.com>
2026-04-01 00:48:33 +0530

    Database Inserting Executed Codes

--- src/main/java/com/example/demo/controllers/GraphQLController.java
+++ src/main/java/com/example/demo/controllers/GraphQLController.java
@@ -1,12 +1,36 @@
 package com.example.demo.controllers;
 
-import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.*;
 import org.springframework.stereotype.Controller;
+
+import com.example.demo.model.Problem;
+import com.example.demo.services.ProblemService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
 @Controller
 public class GraphQLController {
+    @Autowired
+    private ProblemService service;
+
     @QueryMapping
-    public String introduce(){
-        return "This is GraphQl";
+    public List<Problem> getAllProblems(){
+        return service.getAll();
     }
+
+    @MutationMapping
+    public Problem addProblem(@Argument String platformName,@Argument String questionName,@Argument Integer questionId,@Argument String difficulty, @Argument String link,@Argument String intuition,@Argument String code){
+        Problem p = new Problem();
+        p.setPlatformName(platformName);
+        p.setQuestionName(questionName);
+        p.setQuestionId(questionId);
+        p.setDifficulty(difficulty);
+        p.setIntuition(intuition);
+        p.setCode(code);
+        p.setLink(link);
+        System.out.println("questionId = " + questionId);
+        return service.save(p);
+    }
+
 }
