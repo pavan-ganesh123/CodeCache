b8d521a5cc88d3692c59d6fefa5a3f93ac89e03f
Pavan Bolla <pavan.bolla@pragmaticplay.com>
2026-04-24 18:43:14 +0530

    Added tokens, Login, expiry, Fixed cors and dependencies

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.*;
+
+import java.util.List;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .cors(cors -> {})   
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            );
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedMethods(List.of("GET", "POST", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);  // ✅ important for future JWT
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+}
--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,10 +1,15 @@
 package com.example.demo.services;
 
+import java.util.Optional;
+
+import javax.management.RuntimeErrorException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.security.JwtUtil;
 
 @Service
 public class UserService {
@@ -13,5 +18,28 @@ public class UserService {
     
     public User save(User p){
         return repo.save(p);
+    }
+
+    public String login(String email, String password) {
+        System.out.println("LOGIN API HIT");
+
+        email = email.trim();
+        password = password.trim();
+
+        User user = repo.findByEmail(email)
+                .orElseThrow(() -> {
+                    System.out.println("User NOT FOUND");
+                    return new RuntimeException("User not found");
+                });
+
+
+        if (!user.getPassword().equals(password)) {
+            System.out.println("PASSWORD MISMATCH");
+            throw new RuntimeException("Invalid password");
+        }
+
+        System.out.println("LOGIN SUCCESS");
+
+        return JwtUtil.generateToken(email);
     }
 }
