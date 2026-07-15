a5a9cfd262842395982cf836adc1a836fb702a81
Pavan Bolla <pavanganeshbolla@gmail.com>
2026-07-15 16:35:52 +0530

    Wrong URL correction

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("https://codecache-13ic.onrender.com"));
+        config.setAllowedOrigins(List.of("https://adorable-basbousa-4829bb.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
